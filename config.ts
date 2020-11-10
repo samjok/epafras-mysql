bb9364b36c360515c5483cd36f21818ff9ef03c6
Samuel Koivisto <samuel.j.koivisto@gmail.com>
2020-11-10 10:38:13 +0200

    configuration file

---
+++ config.ts
@@ -0,0 +1,19 @@
+import { User } from './entities/User'
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+import "dotenv/config";
+
+const config: MysqlConnectionOptions = {
+    type: "mysql",
+    host: String(process.env.DB_HOST),
+    port: Number(process.env.DB_PORT),
+    username: String(process.env.DB_USER),
+    password: String(process.env.DB_PASSWORD),
+    database: String(process.env.DB_DATABASE),
+    synchronize: true,
+    logging: false,
+    entities: [
+        User
+    ]
+};
+
+export { config }

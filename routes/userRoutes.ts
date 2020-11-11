bd7da7c5fc957716e0e79663634635df04cf0f19
Samuel Koivisto <44672239+samjok@users.noreply.github.com>
2020-11-11 20:53:02 +0200

    Update userRoutes.ts

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -3,7 +3,6 @@ import { addUser, getAllUsers } from "../controllers/userControllers";
 
 const userRoutes = (): Router => {
     const router = Router();
-    router.get("/", getAllUsers);
     router.post("/signup", addUser)
     return router;
 };
